26a100d4bf5b74a76dae00b5908be174245d70d1
user1 <user01@em/com>
2025-05-19 19:04:34 +0300

    added card file

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,8 +2,11 @@
 
 function Cards() {
   return (
-    <div className="">
-        <div>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis ipsa autem ex, atque quis dignissimos sunt eligendi amet ducimus quibusdam praesentium odit nostrum voluptas unde temporibus laudantium sint blanditiis consequuntur.</div>
+    <div className="bg-dark text-light">
+        <div style={{height:"25vh"}}></div>
+        <div className="p-3 m-3">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus voluptatem distinctio quibusdam. Pariatur, consectetur voluptate aliquam debitis nam, autem, ullam animi sed iure libero nihil quia! Quam perferendis molestiae commodi!
+        </div>
     </div>
   )
 }
