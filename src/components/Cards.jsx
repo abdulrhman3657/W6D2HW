95f6c4ea4b22940ded68bd07cf4929a4ce6d5e61
user1 <user01@em/com>
2025-05-19 19:22:57 +0300

    c3

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,7 @@
 function Cards() {
   return (
     <div className="bg-dark text-light">
-        <div style={{height:"25vh"}}></div>
+        {/* <div style={{height:"25vh"}}></div> */}
         <div className="p-3 m-3">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus voluptatem distinctio quibusdam. Pariatur, consectetur voluptate aliquam debitis nam, autem, ullam animi sed iure libero nihil quia! Quam perferendis molestiae commodi!
         </div>
